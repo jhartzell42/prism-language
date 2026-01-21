a30182c886b7122b9c5e16b72d3e87b15af8756b
Jimmy Hartzell <jah259@cornell.edu>
2026-01-20 22:45:10 -0500

    add generic version of tag

--- prism_runtime/src/event/tag.rs
+++ prism_runtime/src/event/tag.rs
@@ -3,7 +3,7 @@ use std::sync::{Arc, Mutex, Weak};
 use crate::{
     behavior::Behavior,
     event::{
-        Event, EventImpl,
+        Event, EventCallback, EventImpl,
         subscriber_list::{SubscriptionEvent, SubscriptionManager},
     },
     runtime::Runtime,
@@ -18,25 +18,41 @@ impl<A: 'static> Event<A> {
     /// tagged value that happened during this occurrence. Promptness is
     /// basically never what you want anyway.
     pub fn tag<B: 'static>(&self, behavior: impl Into<Behavior<B>>) -> Event<(Arc<A>, Arc<B>)> {
+        self.tag_map(|a, b| Some(Arc::new((a, b))), behavior.into())
+    }
+
+    /// [`Event::tag()`], but with a custom function to combine the event and the behavior,
+    /// which can also control whether or not the event fires.
+    ///
+    /// Also not prompt.
+    pub fn tag_map<B: 'static, O: 'static>(
+        &self,
+        function: impl Fn(Arc<A>, Arc<B>) -> Option<Arc<O>> + 'static,
+        behavior: impl Into<Behavior<B>>,
+    ) -> Event<O> {
         Event(Arc::new_cyclic(|weak| Tag {
             event: self.clone(),
             behavior: behavior.into(),
             subscriber_list: SubscriptionManager::new(()),
+            function,
             height: Mutex::new(None),
             weak_self: weak.clone(),
         }))
     }
 }
 
-struct Tag<A: 'static, B: 'static> {
-    subscriber_list: SubscriptionManager<(Arc<A>, Arc<B>), Self>,
+struct Tag<A: 'static, B: 'static, O: 'static, F: 'static + Fn(Arc<A>, Arc<B>) -> Option<Arc<O>>> {
+    subscriber_list: SubscriptionManager<O, Self>,
     event: Event<A>,
     behavior: Behavior<B>,
+    function: F,
     height: Mutex<Option<usize>>,
     weak_self: Weak<Self>,
 }
 
-impl<A: 'static, B> SubscriptionEvent<(Arc<A>, Arc<B>)> for Tag<A, B> {
+impl<A: 'static, B, O: 'static, F: Fn(Arc<A>, Arc<B>) -> Option<Arc<O>>> SubscriptionEvent<O>
+    for Tag<A, B, O, F>
+{
     type Inner = A;
     type Tag = ();
 
@@ -48,14 +64,15 @@ impl<A: 'static, B> SubscriptionEvent<(Arc<A>, Arc<B>)> for Tag<A, B> {
         self.subscriber_list
             .notify(self.weak_self.clone(), Some(&self.event), runtime, || {
                 let behavior_value = self.behavior.0.query_for_tag();
-                let combined_value = Arc::new((value, behavior_value));
-                Some(combined_value)
+                (self.function)(value, behavior_value)
             });
     }
 }
 
-impl<A: 'static, B: 'static> EventImpl<(Arc<A>, Arc<B>)> for Tag<A, B> {
-    fn subscribe(&self, cb: Weak<dyn super::EventCallback<(Arc<A>, Arc<B>)>>) {
+impl<A: 'static, B, O: 'static, F: Fn(Arc<A>, Arc<B>) -> Option<Arc<O>>> EventImpl<O>
+    for Tag<A, B, O, F>
+{
+    fn subscribe(&self, cb: Weak<dyn EventCallback<O>>) {
         self.subscriber_list
             .add_subscriber(self.weak_self.clone(), Some(&self.event), cb);
     }
